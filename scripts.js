75a5cb154738493bd2952cf3d54ee6cb21dc9c37
Pedro A. da Silva <pedrosilvafsa12@gmail.com>
2023-07-04 16:45:13 -0300

    Add function to show the result of operations

--- scripts.js
+++ scripts.js
@@ -1,29 +1,49 @@
+const display = document.querySelector("#display");
+let displayValue = "";
+
 let a;
-let operator;
+let op;
 let b;
 
 const digits = document.querySelectorAll(".digit");
 digits.forEach((digit) => digit.addEventListener("click", displayNumber));
 
-let displayValue = "";
+const operators = document.querySelectorAll(".operator");
+operators.forEach((operator) => operator.addEventListener("click", (e) => {
+  if (op) {
+    displaySolution();
+  }
 
-function displayNumber(e) {
-  displayValue += e.target.textContent;
-  
-  const display = document.querySelector("#display");
+  op = e.target.textContent;
+  a = +displayValue;
+  displayValue = "";
+}));
+
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", displaySolution);
+
+function displaySolution() {
+  b = +displayValue;
+
+  displayValue = operate(op, a, b);
   display.textContent = displayValue;
 }
 
-function operate(operator, a, b) {
-  switch (operator) {
-    case "+":
-      return add(a, b);
+function displayNumber(e) {
+  displayValue += e.target.textContent;  
+  display.textContent = displayValue;
+}
+
+function operate(op, a, b) {
+  switch (op) {
+    case "÷":
+      return divide(a, b);
+    case "×":
+      return multiply(a, b);
     case "-":
       return subtract(a, b);
-    case "*":
-      return multiply(a, b);
-    case "/":
-      return divide(a, b);
+    case "+":
+      return add(a, b);
   }
 }
 
